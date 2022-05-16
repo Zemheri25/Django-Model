80cb1ee535b7fafec4e6128043853c9a39fe002a
Zemheri25 <omerzemheri14@gmail.com>
2022-05-16 22:56:56 +0300

    Django-Models working added

---
+++ relations/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Creator, Language,Developers,Frameworks
+
+admin.site.register(Creator)
+admin.site.register(Language)
+admin.site.register(Developers)
+admin.site.register(Frameworks)
+
+# Register your models here.
